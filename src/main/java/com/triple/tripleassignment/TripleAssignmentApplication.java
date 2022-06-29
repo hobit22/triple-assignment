7d7892bbfce586a9695f5210f7fe0739d672ee6e
hobit22 <hobit22@naver.com>
2022-06-29 22:14:23 +0900

    Initial commit

---
+++ src/main/java/com/triple/tripleassignment/TripleAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.triple.tripleassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleAssignmentApplication.class, args);
+    }
+
+}
